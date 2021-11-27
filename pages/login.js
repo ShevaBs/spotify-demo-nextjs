1ad3a7c2c00a4021e863d8057804d536cf96f3a9
Bogdan Shevchenko <bsheva95@gmail.com>
2021-11-27 15:08:49 +0200

    realized authentication with next-auth, created login page

---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import {getProviders, signIn} from "next-auth/react";
+
+export default function login({providers}) {
+
+
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-black gap-4">
+      <img className="w-52 " src="./spotify-logo.png" alt="logo"/>
+      {
+        Object.values(providers).map(provider => (
+          <div key={provider.name}>
+            <button 
+              onClick={() => signIn(provider.id, {callbackUrl: "/"})}
+              className="bg-[#18d860] text-white p-5 rounded-full font-bold">
+              Login with {provider.name}
+            </button>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers
+    }
+  }
+}
