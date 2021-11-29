54213f9da28ee3e82f267ec293923df7c32fe168
Bogdan Shevchenko <bsheva95@gmail.com>
2021-11-29 12:15:26 +0200

    finished sidebar components, added playlists from spotifyApi

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,13 +7,27 @@ import {
   RssIcon
 } from "@heroicons/react/outline";
 import {signOut, useSession} from 'next-auth/react';
+import { useEffect, useState } from "react";
+import useSpotify from "../hooks/useSpotify";
 
 export default function Sidebar() {
+  const spotifyApi = useSpotify();
   const {data: session, status} = useSession();
+  const [playlists, setPlaylists] = useState([]);
 
+  useEffect(() => {
+    if(spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then(data => {
+        setPlaylists(data.body.items);
+      })
+    }
+
+  }, [session, spotifyApi])
+
+  console.log(playlists)
 
   return (
-    <div className="text-gray-500 p-5 text-sm ring-[1px] ring-gray-900 ring-opacity-90 overflow-y-auto h-screen scrollbar-hide">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-auto h-screen scrollbar-hide">
 
       <div className="space-y-4">
         <button 
@@ -50,63 +64,17 @@ export default function Sidebar() {
         <hr className="border-t-[0.1px] border-gray-900"/>
 
         {/* Playlists */}
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
-        <p className="cursor-pointer hover:text-white transition-all">
-          Playlist Name ...
-        </p>
+        {
+          playlists.map(playlist => (
+            <p 
+              key={playlist.id}
+              className="cursor-pointer hover:text-white transition-all">
+              {playlist.name}
+            </p>
+          ))
+        }
+        
+        
       </div>
 
     </div>
